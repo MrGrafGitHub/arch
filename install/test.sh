64e304da2fca964e4e2885530b63653c64180066
MrGraf <uzerandroid22@gmail.com>
2025-06-07 03:34:27 +0300

    Update test.sh

--- install/test.sh
+++ install/test.sh
@@ -3,29 +3,31 @@ set -e
 
 # --- Настройки ---
 HOSTNAME="arch-vm"
-USERNAME="mrgraf"
-PASSWORD="1234"  # желательно заменить или потом сменить
-
-# Разметка диска (пример для /dev/sda)
+USERNAME="user"
+PASSWORD="1234"
 DISK="/dev/sda"
 
-# Разделы
-BOOT_PART="${DISK}1"
-ROOT_PART="${DISK}2"
-
 # --- Разметка диска ---
+# Полностью очищаем диск (желательно для чистой виртуалки)
+sgdisk --zap-all $DISK
 sgdisk -o $DISK
+
+# Создаём EFI раздел 512 MiB с типом ef00
 sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System Partition" $DISK
+
+# Создаём основной Linux раздел на всё остальное место
 sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux Root Partition" $DISK
 
-# Форматирование
-mkfs.fat -F32 $BOOT_PART
-mkfs.ext4 $ROOT_PART
+# Форматируем разделы
+mkfs.fat -F32 "${DISK}1"
+mkfs.ext4 "${DISK}2"
 
-# Монтирование
-mount $ROOT_PART /mnt
+# Монтируем корень
+mount "${DISK}2" /mnt
+
+# Создаём и монтируем EFI
 mkdir -p /mnt/boot
-mount $BOOT_PART /mnt/boot
+mount "${DISK}1" /mnt/boot
 
 # --- Установка базовой системы ---
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager sudo git
