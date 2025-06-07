fe45fd3f3e09c228036cd38f506b599232a736a2
MrGraf <uzerandroid22@gmail.com>
2025-06-07 03:04:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+set -e
 
+MODE="$1"
+
+if [[ "$MODE" == "test" ]]; then
+  echo "⏳ Запуск в режиме ТЕСТ (виртуалка)"
+  bash ./install/test.sh
+elif [[ "$MODE" == "prod" ]]; then
+  echo "⚙️ Запуск в режиме ПРОД (реальное железо)"
+  bash ./install/prod.sh
+else
+  echo "❌ Ошибка: Укажи режим установки: test или prod"
+  echo "Пример: ./install.sh test"
+  exit 1
+fi
