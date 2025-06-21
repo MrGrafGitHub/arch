1649466061266d15f0cdfbe5c28cb2d1204a968f
MrGraf <uzerandroid22@gmail.com>
2025-06-21 07:54:28 +0300

    Update test2.py

--- install/test2.py
+++ install/test2.py
@@ -168,9 +168,14 @@ class InstallerApp(App):
         #  Установка и настройка Limine
         # Создаем файл limine.conf локально
         await self.set_status("Limine: Проверка директории", 38)
-        limine_dir = "/mnt/boot/limine"
-        os.makedirs(limine_dir, exist_ok=True)
-       
+        try:
+            limine_dir = "/mnt/boot/limine"
+            os.makedirs(limine_dir, exist_ok=True)
+        except Exception as e:
+            await self.write_log(f"[red]Ошибка при создании limine.conf: {e}[/red]")
+            raise
+
+        
         await self.set_status("Limine: создание конфига", 40)
         limine_path = os.path.join(limine_dir, "limine.conf")
         with open(limine_path, "w", encoding="utf-8") as f:
@@ -184,11 +189,11 @@ class InstallerApp(App):
                 "modulepath: boot():/initramfs-linux.img\n"
             )
         #  Используем echo для записи в файл, чтобы избежать проблем с интерпретацией символов
-        await self.run_cmd(["arch-chroot", "/mnt", "bash", "-c", f"echo \"{limineconf}\" > /boot/limine/limine.conf"])
-        await self.run_cmd(["cp", "/usr/share/limine/limine-bios.sys", "/boot/limine/"])
-        await self.run_cmd(["cp", "/usr/share/limine/limine-bios-cd.bin", "/boot/limine/"])
-        await self.run_cmd(["cp", "/usr/share/limine/limine-uefi-cd.bin", "/boot/limine/"])
-        await self.run_cmd(["arch-chroot", "/boot", "limine", "bios-install", "/dev/sda"])
+        await self.run_cmd(["arch-chroot", "/mnt", "bash", "-c", f"echo \"limine.conf\" > /boot/limine/limine.conf"])
+        await self.run_cmd(["cp", "/usr/share/limine/limine-bios.sys", "/mnt/boot/limine/"])
+        await self.run_cmd(["cp", "/usr/share/limine/limine-bios-cd.bin", "/mnt/boot/limine/"])
+        await self.run_cmd(["cp", "/usr/share/limine/limine-uefi-cd.bin", "/mnt/boot/limine/"])
+        await self.run_cmd(["arch-chroot", "/mnt/boot", "limine", "bios-install", "/dev/sda"])
 
         #  Установка менеджера входа ly
         self.set_status("Настройка Display Manager", 50)
