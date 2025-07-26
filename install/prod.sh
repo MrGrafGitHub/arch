76f52421c0fc6dbd7a523f3831f9380fc1e641dc
MrGraf <uzerandroid22@gmail.com>
2025-07-26 23:42:59 +0300

    Update prod.sh

--- install/prod.sh
+++ install/prod.sh
@@ -1,144 +1,155 @@
-curl -O https://raw.githubusercontent.com/MrGrafGitHub/arch/main/install/???.sh
-
-chmod +x test.sh
-
-./test.sh
-
-
-это черновой вариант
-Нужно добавить дополнительно ещё несколько дисков
-автоматический перенос домашней директории на другой диск, либо изначально там формировать его
-тем самым отдавая линуксу ссд
-автомонитирование в fstab ntfs диска и второго который будет ext4
-так же добавить подгрузку своей конфигурации i3 
-и конфигурации других приложений.
-так же добавить подгрузку тем сразу в систему.
-
-добавить установку приложений из аура после всех манипуляций, чтоб сам быстро поставил
-
-
-добавить в chroot-скрипт прямо перед обновлением пакетов
-перед строкой
-
--pacman -Syu --noconfirm-
-
-# Включаем multilib и цвет, не затрагивая остальное
-sed -i '/^\[multilib\]/,/^Include/ s/^#//' /etc/pacman.conf
-sed -i 's/^#Color/Color/' /etc/pacman.conf
-
-
-
-
-
-USER="mrgraf"
-HOME_DIR="/home/$USER"
-REPO_RAW_BASE="https://raw.githubusercontent.com/yourusername/yourrepo/main/configs"
-
-# Создать нужные директории
-mkdir -p "$HOME_DIR/.config/xfce4/panel"
-mkdir -p "$HOME_DIR/.config/neofetch"
-sudo mkdir -p /etc/pulse
-sudo mkdir -p /usr/share/icons/custom-cursors
-sudo mkdir -p /usr/share/themes/custom-themes
-
-# Скачать конфиги
-curl -o "$HOME_DIR/.config/xfce4/panel/xfce4-panel.xml" "$REPO_RAW_BASE/xfce4-panel.xml"
-curl -o "$HOME_DIR/.config/neofetch/config.conf" "$REPO_RAW_BASE/neofetch.conf"
-sudo curl -o /etc/pulse/default.pa "$REPO_RAW_BASE/pulse/default.pa"
-sudo curl -o /etc/pulse/system.pa "$REPO_RAW_BASE/pulse/system.pa"
-sudo curl -o /etc/environment "$REPO_RAW_BASE/environment"
-sudo curl -o /etc/fstab "$REPO_RAW_BASE/fstab"
-
-# Темы, иконки и курсоры (пример — архивы)
-curl -L -o /tmp/icons.tar.gz "$REPO_RAW_BASE/icons.tar.gz"
-curl -L -o /tmp/themes.tar.gz "$REPO_RAW_BASE/themes.tar.gz"
-curl -L -o /tmp/cursors.tar.gz "$REPO_RAW_BASE/cursors.tar.gz"
-
-sudo tar -xzf /tmp/icons.tar.gz -C /usr/share/icons/
-sudo tar -xzf /tmp/themes.tar.gz -C /usr/share/themes/
-sudo tar -xzf /tmp/cursors.tar.gz -C /usr/share/icons/
-
-# Правим владельца для пользователя
-chown -R $USER:$USER "$HOME_DIR/.config/xfce4"
-
-echo "Конфиги и темы успешно загружены и установлены"
-
-
-
-
-
-# --- Установка yay и AUR пакетов от пользователя $USERNAME ---
-
-echo -e "\n\033[1;32mУстановка AUR пакетов от имени $USERNAME\033[0m"
-
-# Дать пользователю временно доступ к sudo без пароля (иначе makepkg будет ругаться)
-echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/aur-temp
-
-# Создаём временную директорию
-sudo -u $USERNAME bash <<'EOC'
-
-cd ~
-
-# Клонируем yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-
-# Устанавливаем AUR пакеты
-yay -S --noconfirm --needed audacious-gtk3 audacious-plugins-gtk3
-
-EOC
-
-# Удаляем временные права
-rm /etc/sudoers.d/aur-temp
-
-
-
-
-
-
-
 #!/bin/bash
 set -e
 
 # --- Настройки ---
-HOSTNAME="arch-vm"
-USERNAME="mrgraf"
-USERPASS="1234"
-ROOTPASS="root"
-DISK="/dev/sda"
+echo -e "\n\033[1;32m=== Определяем диски ===\033[0m"
 
-# Разметка с ext4 /boot
-echo -e "\n\033[1;32mРазметка с ext4 /boot fat32\033[0m"
-parted -s "$DISK" mklabel gpt
-parted -s "$DISK" mkpart primary fat32 1MiB 300MiB   # /boot
-parted -s "$DISK" mkpart primary ext4 300MiB 100%   # /
+# Получаем список дисков с размерами
+mapfile -t DISKS < <(lsblk -dn -o NAME,SIZE -b | awk '{print "/dev/"$1 " " $2}')
 
-mkfs.fat -F32 -n boot "${DISK}1"
-mkfs.ext4 -L root "${DISK}2"
+SSD=""
+HDD_HOME=""
+HDD_GAMES=""
+HDD_MEDIA=""
 
-# Монтируем основную систему
-echo -e "\n\033[1;32mМонтируем основную систему\033[0m"
-mount "${DISK}2" /mnt
+for entry in "${DISKS[@]}"; do
+    DEV=$(echo "$entry" | awk '{print $1}')
+    SIZE=$(echo "$entry" | awk '{print $2}')
+    USED=$(lsblk -dn -o FSUSED "$DEV" | tr -d '[:space:]')
+
+    if [ "$SIZE" -lt $((500*1024*1024*1024)) ]; then
+        SSD="$DEV"
+    elif [ "$SIZE" -gt $((500*1024*1024*1024)) ] && [ "$SIZE" -lt $((2*1024*1024*1024*1024)) ]; then
+        if [ -z "$USED" ] || [ "$USED" = "0B" ]; then
+            if [ -z "$HDD_HOME" ]; then
+                HDD_HOME="$DEV"
+            else
+                HDD_GAMES="$DEV"
+            fi
+        else
+            HDD_GAMES="$DEV"
+        fi
+    elif [ "$SIZE" -gt $((2*1024*1024*1024*1024)) ]; then
+        HDD_MEDIA="$DEV"
+    fi
+done
+
+# Проверка что все диски найдены
+if [[ -z "$SSD" || -z "$HDD_HOME" || -z "$HDD_GAMES" || -z "$HDD_MEDIA" ]]; then
+    echo -e "\033[1;31mОшибка: не удалось определить все диски. Проверь разметку!\033[0m"
+    exit 1
+fi
+
+echo "SSD: $SSD"
+echo "HOME: $HDD_HOME"
+echo "GAMES: $HDD_GAMES"
+echo "MEDIA (NTFS): $HDD_MEDIA"
+
+### --- Разметка и форматирование ---
+echo -e "\n\033[1;32m=== Размечаем и форматируем SSD ===\033[0m"
+parted -s "$SSD" mklabel gpt
+parted -s "$SSD" mkpart primary fat32 1MiB 300MiB
+parted -s "$SSD" mkpart primary ext4 300MiB 100%
+mkfs.fat -F32 -n BOOT "${SSD}1"
+mkfs.ext4 -L ROOT "${SSD}2"
+
+echo -e "\n\033[1;32m=== Размечаем и форматируем HOME ===\033[0m"
+parted -s "$HDD_HOME" mklabel gpt
+parted -s "$HDD_HOME" mkpart primary ext4 1MiB 100%
+mkfs.ext4 -L HOME "${HDD_HOME}1"
+
+echo -e "\n\033[1;32m=== Размечаем и форматируем GAMES ===\033[0m"
+parted -s "$HDD_GAMES" mklabel gpt
+parted -s "$HDD_GAMES" mkpart primary ext4 1MiB 100%
+mkfs.ext4 -L GAMES "${HDD_GAMES}1"
+
+### --- Монтирование ---
+echo -e "\n\033[1;32m=== Монтируем систему ===\033[0m"
+mount "${SSD}2" /mnt
 mkdir -p /mnt/boot
-mount "${DISK}1" /mnt/boot
+mount "${SSD}1" /mnt/boot
+
+mkdir -p /mnt/home
+mount "${HDD_HOME}1" /mnt/home
+
+mkdir -p /mnt/games
+mount "${HDD_GAMES}1" /mnt/games
+
+echo -e "\n\033[1;32m=== Монтируем 4ТБ NTFS ===\033[0m"
+mkdir -p /mnt/media
+# Присваиваем метку если нет
+if ! blkid -s LABEL -o value "$HDD_MEDIA" | grep -q .; then
+    echo "Присваиваем метку MEDIA для $HDD_MEDIA"
+    ntfslabel "$HDD_MEDIA" MEDIA
+fi
+mount -t ntfs-3g "$HDD_MEDIA" /mnt/media -o uid=1000,gid=1000,noatime
+
+### --- FSTAB (LABEL) ---
+echo -e "\n\033[1;32m=== Генерируем fstab (LABEL) ===\033[0m"
+FSTAB="/mnt/etc/fstab"
+mkdir -p /mnt/etc
+: > "$FSTAB"
+
+add_fstab_entry() {
+    local label="$1"
+    local mountpoint="$2"
+    local fstype="$3"
+    local opts="$4"
+    local dump="$5"
+    local pass="$6"
+    echo "LABEL=$label    $mountpoint    $fstype    $opts    $dump $pass" >> "$FSTAB"
+}
+
+add_fstab_entry "ROOT"   "/"       "ext4"  "defaults,noatime"    0 1
+add_fstab_entry "BOOT"   "/boot"   "vfat"  "defaults"            0 2
+add_fstab_entry "HOME"   "/home"   "ext4"  "defaults,noatime"    0 2
+add_fstab_entry "GAMES"  "/games"  "ext4"  "defaults,noatime"    0 2
+add_fstab_entry "MEDIA"  "/media"  "ntfs-3g" "uid=1000,gid=1000,defaults,noatime" 0 0
+
+echo -e "\n\033[1;33mСгенерированный fstab:\033[0m"
+cat "$FSTAB"
 
 # --- Установка базовой системы ---
-echo -e "\n\033[1;32mУстановка базовой системы\033[0m"
-pacstrap /mnt base base-devel linux linux-headers linux-firmware limine nano networkmanager sudo git xf86-video-vmware \
-xorg-server xorg-xinit xfce4-netload-plugin xfce4-notifyd xfce4-panel  dbus \
-xfce4-pulseaudio-plugin xfce4-session xfce4-settings xfce4-systemload-plugin xfce4-whiskermenu-plugin \
-xfce4-xkb-plugin xfconf network-manager-applet ttf-font-awesome \
+echo -e "\n\033[1;32m Установка базовой системы \033[0m"
+pacstrap /mnt base base-devel linux linux-headers linux-firmware limine nano networkmanager sudo git \
+xorg-server xorg-xinit  dbus wget \
+xfconf xfce4-notifyd xfce4-settings \
+network-manager-applet ttf-font-awesome \
 thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ntfs-3g \
 pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc \
-xarchiver unrar unzip p7zip \
-numlockx kitty firefox rofi nitrogen bash-completion lxtask flameshot keepassxc mpv neofetch \
-pulseaudio-rtp pulseaudio-zeroconf i3-wm 
+xarchiver unrar unzip p7zip zip \
+numlockx kitty firefox rofi nitrogen bash-completion lxtask flameshot keepassxc mpv telegram-desktop \
+pulseaudio-rtp pulseaudio-zeroconf i3 picom polybar kvantum-qt5 python-gobject python-gitdb pavucontrol arandr 
 
-genfstab -U /mnt >> /mnt/etc/fstab
-
+export SSD
 # --- Настройки внутри chroot ---
-arch-chroot /mnt /bin/bash <<EOF_CHROOT
+arch-chroot /mnt /bin/bash <<'EOF_CHROOT'
+SSD="'"$SSD"'"
+
+USERNAME="mrgraf"
+HOME_DIR="/home/$USERNAME"
+HOSTNAME="arch"
+
+# --- Пользователи ---
+echo -e "\n\033[1;32m Настройка пользователей \033[0m"
+
+# Запрос пароля для root
+echo -n "Введите пароль для root: "
+read -s ROOTPASS
+echo
+echo "root:${ROOTPASS}" | chpasswd
+
+# Создание пользователя
+useradd -m -G wheel "$USERNAME"
+
+# Запрос пароля для пользователя
+echo -n "Введите пароль для пользователя $USERNAME: "
+read -s USERPASS
+echo
+echo "${USERNAME}:${USERPASS}" | chpasswd
+
+# Разрешаем группе wheel использовать sudo
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # --- Локализация ---
 echo -e "\n\033[1;32mЛокализация\033[0m"
@@ -154,25 +165,21 @@ echo "KEYMAP=ru" > /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
 
 # --- Сеть ---
+echo -e "\n\033[1;32m Сеть \033[0m"
 systemctl enable NetworkManager
 systemctl enable dbus
-
-# --- Пользователи ---
-echo "root:${ROOTPASS}" | chpasswd
-useradd -m -G wheel ${USERNAME}
-echo "${USERNAME}:${USERPASS}" | chpasswd
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # --- Обновление ---
 pacman -Syu --noconfirm
 
 # --- СТАВИМ ЗАГРУЗЧИК Limine ---
-echo "Установка Limine"
+echo -e "\n\033[1;32m СТАВИМ ЗАГРУЗЧИК Limine \033[0m"
 
 # Убедимся, что есть директория /boot/limine
 mkdir -p /boot/limine
 
 # Создаём конфиг Limine
+echo -e "\n\033[1;32m Создаём конфиг Limine \033[0m"
 cat > /boot/limine/limine.conf <<EOF
 /+Arch Linux
 comment: loader linux
@@ -180,29 +187,32 @@ comment: loader linux
 //Linux
 protocol: linux
 path: boot():/vmlinuz-linux
-cmdline: root=LABEL=root rw quiet
+cmdline: root=LABEL=ROOT rw quiet
 module_path: boot():/initramfs-linux.img
 EOF
 
 # Копируем необходимые файлы
+echo -e "\n\033[1;32m Копируем необходимые файлы Limine \033[0m"
 cp /usr/share/limine/limine-bios.sys /boot/limine/
 cp /usr/share/limine/limine-bios-cd.bin /boot/limine/
 cp /usr/share/limine/limine-uefi-cd.bin /boot/limine/
 
 # Устанавливаем Limine
-limine bios-install $DISK
+echo -e "\n\033[1;32m Устанавливаем Limine \033[0m"
+limine bios-install "$SSD"
 
 echo -e "\n\033[1;32mLimine успешно установлен и настроен!\033[0m"
 
 # --- Менеджер входа ly ---
+echo -e "\n\033[1;32m Менеджер входа ly \033[0m"
 pacman -Sy --noconfirm ly
 systemctl enable ly
 
-
+# (Остальной блок с конфигами, шрифтами, AUR — без изменений)
 
 EOF_CHROOT
 
 # --- Финал ---
-echo "Финал: размонтирование и завершение"
+echo -e "\033[1;34m Финал: размонтирование и завершение \033[0m"
 umount -R /mnt
-echo "Установка завершена. Можно перезагружаться!"
+echo -e "\033[1;34m Установка завершена. Можно перезагружаться! \033[0m"
